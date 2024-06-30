0724ec08c6bd612ce1fd5bb16711a6b7e9c17701
sedomeenz <sedomeenz@gmail.com>
2024-07-01 00:14:41 +0330

    Part 1
    - Create main page  &  CSS file
    - Create login form  &  CSS file
        - Set cookies for 1 user
    - Create user panel  &  CSS file
    - Design logout page without  logout feature
    - Create and design redirecting page

---
+++ Main_page.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Main page | CyberReport</title>
+    <link rel="stylesheet" href=".//CSS_files//main.css">
+</head>
+<body>
+    <header>
+        <h1> CyberReport </h1>
+        <nav>
+            <ul>
+                <li><a href="#">Write</a></li>
+                <li><a href="#">Posts</a></li>
+                <li><a href="#">Signup|Login</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <section>
+            <h2>About Us</h2>
+            <p>This website build for member of Ethicall Hackers coumionity.</p>
+        </section>
+        <section>
+            <h2>Our Services</h2>
+            <p>You can send us your <a style="color: chartreuse;" href="telegram">REPORT</a> and write posts.</p>
+        </section>
+    </main>
+    <footer>
+        <p>&copy; 2024 CyberReport Website. All rights reserved.</p>
+    </footer>
+</body>
+</html>
